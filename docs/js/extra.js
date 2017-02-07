700ae62c5c22cdccaf59eb817bf6246e73ed444e
weelion <weelion@qq.com>
2017-02-07 16:25:19 +0800

    update

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,1 +1,18 @@
-$('link').eq(0).remove();
+  // google analytics
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-90622135-1', 'auto');
+  ga('send', 'pageview');
+
+
+  // baidu analytics
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?972312727f90f9bcf094b37a20acbfca";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
